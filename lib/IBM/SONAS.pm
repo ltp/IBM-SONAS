63f43fec9187d11bcc733a271b03234c99e16e02
Luke Poskitt <luke.poskitt@gmail.com>
2015-11-27 08:38:48 +1100

    Version 0.20

--- lib/IBM/SONAS.pm
+++ lib/IBM/SONAS.pm
@@ -6,7 +6,7 @@ use warnings;
 use IBM::StorageSystem;
 use Carp qw(croak);
 
-our $VERSION = '0.024';
+our $VERSION = '0.20';
 
 our @METHODS = qw(health disk export filesystem interface mount node quota 
 replication service snapshot task);
